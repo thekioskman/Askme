5e542f2c15e23989b4047151b601dc234ee8cf1f
Brian Qiu <brian.qiu9522@hotmail,com>
2020-10-11 22:37:53 -0400

    Updates

---
+++ precache-manifest.1ffc3db8389b9eebe94a6e16af19879e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5151ebc175272139107e58ef9b4a4f94",
+    "url": "/thekioskman/Askme.git/index.html"
+  },
+  {
+    "revision": "c88240d1414d2966b700",
+    "url": "/thekioskman/Askme.git/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "0f5373616f2c46d4e9b4",
+    "url": "/thekioskman/Askme.git/static/js/2.ff321ca0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thekioskman/Askme.git/static/js/2.ff321ca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c88240d1414d2966b700",
+    "url": "/thekioskman/Askme.git/static/js/main.78012554.chunk.js"
+  },
+  {
+    "revision": "5c6d2966748ec915b6cf",
+    "url": "/thekioskman/Askme.git/static/js/runtime-main.915b9c5c.js"
+  }
+]);
